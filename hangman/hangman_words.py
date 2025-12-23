fda68c8c1af0c4e2f65a9218dafbee3dfb1c40df
siddharth525 <siddharth@elred.io>
2025-12-23 16:52:30 +0530

    Hangman game added

---
+++ hangman/hangman_words.py
@@ -0,0 +1,19 @@
+word_list = [
+    "alpha", "bravo", "charlie", "delta", "echo",
+    "foxtrot", "golf", "hotel", "india", "juliet",
+    "kilo", "lima", "mike", "november", "oscar",
+    "papa", "quebec", "romeo", "sierra", "tango",
+    "uniform", "victor", "whiskey", "xray", "yankee",
+    "zulu", "apple", "banana", "cherry", "date",
+    "elderberry", "fig", "grape", "honeydew", "iceberg",
+    "jackfruit", "kiwi", "lemon", "mango", "nectarine",
+    "orange", "papaya", "quince", "raspberry", "strawberry",
+    "tomato", "ugli", "vanilla", "watermelon", "xigua",
+    "yam", "zucchini", "red", "blue", "green",
+    "yellow", "purple", "black", "white",
+    "gray", "pink", "brown", "cyan", "magenta",
+    "silver", "gold", "bronze", "circle", "square",
+    "triangle", "rectangle", "polygon", "line", "curve",
+    "point", "vector", "matrix", "scalar", "tensor",
+    "array", "list", "tuple", "set", "dictionary"
+]
